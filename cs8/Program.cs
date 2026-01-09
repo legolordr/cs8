ee4deacbf2a788af0074a96b4894f3d4c7044e31
legolordr <Ishmametev_den@mail.ru>
2026-01-09 18:13:57 +0500

    ex handling

--- cs8/Program.cs
+++ cs8/Program.cs
@@ -8,14 +8,29 @@ public static class Program
     private static async Task Main()
     {
         var http =  new HttpClient();
-        string id = Input.InputId();
-        CreationUrl url = new CreationUrl(id);
-        var json = await http.GetFromJsonAsync<SiteData>(url.FullUrl);
-        var options = new JsonSerializerOptions
+        while (true)
         {
-            WriteIndented = true,
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
-        };
-        Console.WriteLine(JsonSerializer.Serialize(json, options));
+            try
+            {
+                string id = Input.InputId();
+                CreationUrl url = new CreationUrl(id);
+                var json = await http.GetFromJsonAsync<SiteData>(url.FullUrl);
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                };
+                Console.WriteLine(JsonSerializer.Serialize(json, options));
+            }
+            catch (HttpRequestException)
+            {
+                Console.WriteLine("You entered an invalid id");
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("Invalid json");
+                throw;
+            }
+        }
     }
 }
