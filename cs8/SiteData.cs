43f952d8c91fee8043c15e024146bca16ea52bc2
legolordr <Ishmametev_den@mail.ru>
2026-01-20 17:39:59 +0500

    inheritance

--- cs8/SiteData.cs
+++ cs8/SiteData.cs
@@ -14,23 +14,14 @@ public class Lineage
     public DateTimeOffset UpdatedAt { get; set; }
 }
 
-public class Root
+public class Root : Lineage
 {
     public string AccessLevel { get; set; }
     public Share Share { get; set; }
     public List<Lineage> Lineage { get; set; }
     public List<Lineage> Children { get; set; }
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public bool IsIndexed { get; set; }
-    public string CoverUrl { get; set; }
-    public string OwnerId { get; set; }
     public string ParentId { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
-    public DateTimeOffset UpdatedAt { get; set; }
 }
-
 public class Share
 {
     public string Access { get; set; }
